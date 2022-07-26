f3d992bc2793ba34cf24b3324efe8f204ac95063
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-07-26 20:02:23 +1000

    JDBC as a service provider framework added

--- src/main/java/item1/ServiceProviderFramework/Main.java
+++ src/main/java/item1/ServiceProviderFramework/Main.java
@@ -1,7 +1,39 @@
 package item1.ServiceProviderFramework;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
-        
+        /* JDBC is a service provider framework. */
+        try {
+            /*
+            * DriverManager.registerDriver is the provider registration.
+            * Driver is the service provider interface.
+            */
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            /*
+            * Connection is the service interface.
+            * DriverManager.getConnection is the service access API.
+            */
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbname\",\"username\",\"userpassword");
+        } catch (Exception e) {}
+    }
+}
+
+interface MyService {
+    void doSomething();
+}
+
+class MyServiceFactory {
+    public static MyService getService() {
+        try {
+            MyService myService = (MyService) Class.forName("com.mysql.jdbc.Driver").newInstance();
+            myService.doSomething();
+        }
+        catch (Throwable t) {
+            throw new Error(t);
+        }
+        return null;
     }
 }
