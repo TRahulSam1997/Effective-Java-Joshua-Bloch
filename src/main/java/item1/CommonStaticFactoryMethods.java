58886385a0b0e380e817e7d35490f5814b33984e
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-07-28 20:46:01 +1000

    commonly used static factory methods added

--- src/main/java/item1/CommonStaticFactoryMethods.java
+++ src/main/java/item1/CommonStaticFactoryMethods.java
@@ -1,7 +1,35 @@
 package item1;
 
+import java.math.BigInteger;
+import java.time.Instant;
+import java.util.Date;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+
+//class Rank{ }
+enum Rank {
+    JACK,
+    QUEEN,
+    KING
+}
 public class CommonStaticFactoryMethods {
+    private static final Rank JACK = null;
+    private static final Rank QUEEN = null;
+    private static final Rank KING = null;
+
     public static void main(String[] args) {
-        
+        /* A type-conversion method. */
+        Instant inst = Instant.now();
+        Date date = Date.from(inst);
+        System.out.println("date -> " + date);
+
+        /* An aggregation method. */
+        Set<Rank> faceCards = EnumSet.of(Rank.JACK, Rank.QUEEN, Rank.KING);
+        System.out.println("faceCards -> " + faceCards);
+
+        /* A verbose alternative to from and of. */
+        BigInteger prime = BigInteger.valueOf(Integer.MAX_VALUE);
+        System.out.println("prime -> " + prime);
     }
 }
