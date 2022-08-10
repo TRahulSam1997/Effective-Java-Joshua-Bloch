45b307cab3d8bb8754a66e73ae5fce086ec7a93a
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-08-10 20:04:38 +1000

    JavaBeans pattern added

--- src/main/java/item2/Main.java
+++ src/main/java/item2/Main.java
@@ -1,11 +1,10 @@
 package item2;
 
 import item2.telescopingconstructor.NutritionFacts;
+import item2.javabeans.NutritionFactsJB;
 
 public class Main {
     /* Instantiating with the Telescope constructor pattern. */
     NutritionFacts coke = new NutritionFacts(240, 8, 100, 0, 35, 27);
     NutritionFacts bundaberg = new NutritionFacts(360, 2, 150);
-
-
 }
---
+++ src/main/java/item2/javabeans/NutritionFactsJB.java
@@ -0,0 +1,34 @@
+package item2.javabeans;
+
+/*
+ * https://github.com/jbloch/effective-java-3e-source-code/blob/master/src/effectivejava/chapter2/item2/javabeans/NutritionFacts.java
+ * JavaBeans Pattern - allows inconsistency, mandates mutability  (pages 11-12)
+ */
+public class NutritionFactsJB {
+    // Parameters initialized to default values (if any)
+    private int servingSize  = -1; // Required; no default value
+    private int servings     = -1; // Required; no default value
+    private int calories     = 0;
+    private int fat          = 0;
+    private int sodium       = 0;
+    private int carbohydrate = 0;
+
+    public NutritionFactsJB() { }
+    // Setters
+    public void setServingSize(int val)  { servingSize = val; }
+    public void setServings(int val)     { servings = val; }
+    public void setCalories(int val)     { calories = val; }
+    public void setFat(int val)          { fat = val; }
+    public void setSodium(int val)       { sodium = val; }
+    public void setCarbohydrate(int val) { carbohydrate = val; }
+
+    /* Creating an object with JavaBeans pattern. */
+    public static void main(String[] args) {
+        NutritionFactsJB cocaCola = new NutritionFactsJB();
+        cocaCola.setServingSize(240);
+        cocaCola.setServings(8);
+        cocaCola.setCalories(100);
+        cocaCola.setSodium(35);
+        cocaCola.setCarbohydrate(27);
+    }
+}
