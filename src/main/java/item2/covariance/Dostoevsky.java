c06ea3ff9ce15c6085e68885f71ac55a7b2db034
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-08-27 17:14:20 +1000

    covariant return type example

--- src/main/java/item2/covariance/Dostoevsky.java
+++ src/main/java/item2/covariance/Dostoevsky.java
@@ -1,9 +1,11 @@
 package item2.covariance;
 
-public class Dostoevsky extends Author{
+public class Dostoevsky extends Author {
     private String name = "Fyodor Dostoevsky";
 
-    /* This is covariant. */
+    /* This is covariant: while it returns a subclass of the return type (Object -> String)
+     * it still fullfills the contract of Author.getAuthorName().
+     */
     @Override
     public String getAuthorName() {
         return name;
