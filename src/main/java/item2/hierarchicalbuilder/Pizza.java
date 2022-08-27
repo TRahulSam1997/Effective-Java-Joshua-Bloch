dc570f5ae006e1476520b22a76dbdc22266f7027
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-08-27 16:32:46 +1000

    method chaining example

--- src/main/java/item2/hierarchicalbuilder/Pizza.java
+++ src/main/java/item2/hierarchicalbuilder/Pizza.java
@@ -10,6 +10,7 @@ public abstract class Pizza {
     public enum Topping { HAM, MUSHROOM, ONION, PEPPER, SAUSAGE }
     final Set<Topping> toppings;
 
+    /* Allows simulated self-type. */
     abstract static class Builder<T extends Builder<T>> {
         EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
         public T addTopping(Topping topping) {
@@ -20,9 +21,11 @@ public abstract class Pizza {
         abstract Pizza build();
 
         // Subclasses must override this method to return "this"
+        /* Overriding this method allows subclasses to get an instance of their own type. */
         protected abstract T self();
     }
 
+    /* The generic, recursive type parameter is necessary for method chaining to work. */
     Pizza(Builder<?> builder) {
         /* Defensive copy been made to ensure encapsulation isn't broken.
          * When the native class changes a mutable object's field, a defensive copy should be made any time it's passed into a constructor like this or out of the class.
