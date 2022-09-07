b60cf69d6c631439e98ba5b81863ace1f30e76ed
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-09-07 19:09:14 +1000

    generic singleton pattern added

--- src/main/java/Item3/genericsingletonfactory/GenericSingletonFactory.java
+++ src/main/java/Item3/genericsingletonfactory/GenericSingletonFactory.java
@@ -8,6 +8,7 @@ import java.util.function.UnaryOperator;
  */
 public class GenericSingletonFactory {
     // Generic singleton factory pattern
+    /* Object is the superclass for all the classes, therefore all types passed will be a subtype. */
     private static final UnaryOperator<Object> IDENTITY_FN = (t) -> t;
 
     @SuppressWarnings("unchecked")
@@ -17,14 +18,21 @@ public class GenericSingletonFactory {
 
     // Sample program to exercise generic singleton
     public static void main(String[] args) {
+        /* Can be used without casts. */
         String[] strings = { "jute", "hemp", "nylon" };
         UnaryOperator<String> sameString = identityFunction();
-        for (String s : strings)
+        System.out.println("Same object for UnaryOperator<String> -> " + sameString);
+        for (String s : strings) {
+            // Applying the function to get the result
             System.out.println(sameString.apply(s));
+        }
 
         Number[] numbers = { 1, 2.0, 3L };
         UnaryOperator<Number> sameNumber = identityFunction();
-        for (Number n : numbers)
+        System.out.println("Same object for UnaryOperator<Number> -> " + sameNumber);
+        for (Number n : numbers) {
+            // Applying the function to get the result
             System.out.println(sameNumber.apply(n));
+        }
     }
 }
