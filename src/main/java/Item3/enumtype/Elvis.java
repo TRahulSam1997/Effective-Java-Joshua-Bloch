22146a3ecb0fd1b5019df9e5bddd3c0971464f15
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-09-23 22:10:39 +1000

    effective java joshua bloch

--- src/main/java/Item3/enumtype/Elvis.java
+++ src/main/java/Item3/enumtype/Elvis.java
@@ -5,7 +5,6 @@ package Item3.enumtype;
  */
 public enum Elvis {
     INSTANCE;
-
     public void leaveTheBuilding() {
         System.out.println("Whoa baby, I'm outta here!");
     }
---
+++ src/main/java/Item4/UtilityClass.java
@@ -0,0 +1,14 @@
+package Item4;
+
+/*
+ * Code taken from - https://github.com/jbloch/effective-java-3e-source-code/blob/master/src/effectivejava/chapter2/item4/UtilityClass.java
+ * Noninstantiable utility class (Page 19)
+ */
+
+public class UtilityClass {
+    // Suppress default constructor for noninstantiability
+    private UtilityClass() {
+        throw new AssertionError();
+    }
+
+}
