f38e1c22967a69c9a908e0619c1e8e778871dbcf
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-10-04 17:41:50 +1100

    clarifying comments added

--- src/main/java/Item5/dependencyinjection/SpellChecker.java
+++ src/main/java/Item5/dependencyinjection/SpellChecker.java
@@ -10,7 +10,9 @@ import java.util.Objects;
 public class SpellChecker {
     private final Lexicon dictionary;
 
-    /* Dependent object (reference) is passed here. */
+    /* Dependent object (reference) is passed here.
+     * We rely on the client to provide implementation.
+     */
     public SpellChecker(Lexicon dictionary) {
         /* Checks if the supplied object reference (dictionary, in this case) is null.
          * If it's null, then a NullPointerException is thrown.
