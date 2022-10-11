2f72f78fde89ac56f3f21117664dd673bf26fb79
Rahul <rahul.samaranayake@zendesk.com>
2022-10-11 21:12:58 +1100

    demo file update

--- src/main/java/Item6/Demo.java
+++ src/main/java/Item6/Demo.java
@@ -1,7 +1,25 @@
 package Item6;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Demo {
     public static void main(String[] args) {
         System.out.println(Boolean.valueOf("true"));
+    }
+}
+
+/* Adapter example. */
+class keySetDemo {
+    public static void main(String[] args) {
+        Map<String, String> capitalCities = new HashMap<String, String>();
+
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+
+        /*Returned objects are functionally identical.*/
+        System.out.println(capitalCities.keySet());
     }
 }
