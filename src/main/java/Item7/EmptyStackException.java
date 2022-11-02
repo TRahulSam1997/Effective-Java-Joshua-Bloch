362c25074e60f78d60c873fdc057997d7551c3f8
Rahul <rahul.samaranayake@zendesk.com>
2022-11-02 19:50:50 +1100

    Item 7 added

--- src/main/java/Item7/EmptyStackException.java
+++ src/main/java/Item7/EmptyStackException.java
@@ -1,5 +1,9 @@
 package Item7;
 
-public class EmptyStackException {
+/*
+ * Code taken from - https://github.com/jbloch/effective-java-3e-source-code/blob/master/src/effectivejava/chapter2/item7/EmptyStackException.java
+ * (Thrown by Stack program on Page 26)
+ */
 
+public class EmptyStackException extends IllegalStateException {
 }
