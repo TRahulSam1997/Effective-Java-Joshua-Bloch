d6975d877aae55d1f3870d1314b3ed9b0b195a63
Rahul <rahul.samaranayake@zendesk.com>
2022-11-09 20:29:49 +1100

    weak hashmap demo

--- src/main/java/Item7/WeakHashMap/WeakHashMapDemo.java
+++ src/main/java/Item7/WeakHashMap/WeakHashMapDemo.java
@@ -1,6 +1,8 @@
 package Item7.WeakHashMap;
 
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.WeakHashMap;
 
 /** Example courtesy of Baeldung */
 public class WeakHashMapDemo {
@@ -15,5 +17,36 @@ public class WeakHashMapDemo {
          * as there is no other strong reference pointing to it.
          */
         prime = null;
+
+        /** Ideally, we want a Map implementation that allows GC to automatically delete unused objects.
+         * When a key of a big image object is not in use in our application in any place,
+         * that entry will be deleted from memory.
+         */
+        WeakHashMap<UniqueImageName, BigImage> wMap = new WeakHashMap<>();
+        HashMap<UniqueImageName, BigImage> map = new HashMap<>();
+        BigImage bigImage = new BigImage("image_id");
+        UniqueImageName imageName = new UniqueImageName("name_of_big_image");
+
+        /** We are putting a BigImage object as a value and an imageName object reference as a key. */
+        wMap.put(imageName, bigImage);
+        System.out.println("Does Map contains imageName -> " + wMap.containsKey(imageName));
+
+        wMap.put(imageName, bigImage);
+
+        /** The key is set to null. */
+        imageName = null;
+        /** Forcing JVM to GC. */
+        System.gc();
+        System.out.println("Weak Map is empty -> " + wMap);
+    }
+
+    private static class UniqueImageName {
+        public UniqueImageName(String name_of_big_image) {
+        }
+    }
+
+    private static class BigImage {
+        public BigImage(String image_id) {
+        }
     }
 }
