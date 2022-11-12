68edb458852f0d99534b9eeea4d984227a3d9215
Rahul <rahul.samaranayake@zendesk.com>
2022-11-12 16:00:15 +1100

    callbacks with weak reference demo

--- src/main/java/Item7/callbacks/CallBacksDemo.java
+++ src/main/java/Item7/callbacks/CallBacksDemo.java
@@ -3,34 +3,52 @@ package Item7.callbacks;
 import java.util.WeakHashMap;
 
 /** Example courtesy of Ingo Kegel */
+
+/**
+ * Since Java doesn't support function pointers this represents a callback.
+ * Effectively, It's a reference to the function which will be called.
+ */
+interface Listener {
+    void event();
+}
+
+/** CallBacksDemo represents the mock API user. */
 public class CallBacksDemo {
-    Listener listener;
-    void registerListener() {
+    Listener listener = null;
+    public void registerListener() {
+
+        /** The client of the API maintains a strong reference. */
         listener = new Listener() {
             @Override
             public void event() {
-                // TODO
+                System.out.println("Some task... ");
             }
         };
+
+        /** The observer registers itself. */
         Api.registerListener(listener);
     }
 
-    class Api {
-        static WeakHashMap<Listener, Object> listeners = new WeakHashMap<>();
-        static void registerListener(Listener listener) {
-            listeners.put(listener, null);
-        }
+    /** This class represents the subject in the Observer Pattern. */
+}
 
-        static void fireListeners() {
-            for (Listener listener : listeners.keySet()) {
-                if (listener != null) {
-                    listener.event();
-                }
-            }
-        }
+class Api {
+
+    /**
+     * The API maintains a weak reference.
+     * When the client is garbage collected, the listener is also garbage collected.
+     */
+    static WeakHashMap<Listener, Object> listeners = new WeakHashMap<>();
+    static void registerListener(Listener listener) {
+        listeners.put(listener, null);
     }
 
-    interface Listener {
-        void event();
+    static void fireListeners() {
+        for (Listener listener : listeners.keySet()) {
+            /** Checks if the listener is registered. */
+            if (listener != null) {
+                listener.event();
+            }
+        }
     }
 }
