d82ebd8c60e441088469cf785beee89aa1779283
Rahul <rahul.samaranayake@zendesk.com>
2022-11-25 22:29:41 +1100

    finalisers security flaw demo

--- src/main/java/Item8/finalisers/securityproblem/FinalizerAttackExample.java
+++ src/main/java/Item8/finalisers/securityproblem/FinalizerAttackExample.java
@@ -1,7 +1,7 @@
 package Item8.finalisers.securityproblem;
 
 public class FinalizerAttackExample {
-    /** Inner class - */
+    /** Inner class - objects created by this should "die on the vine" if an exception is thrown. */
     static class ResourceClass {
         ResourceClass() {
             if(!checkCaller()) throw new SecurityException();
@@ -18,21 +18,29 @@ public class FinalizerAttackExample {
     }
 
     static class MaliciousSubclass extends ResourceClass {
+        /** There is no need to store something in a static variable.
+         * As soon as the finalize() method is invoked, the object is resurrected.
+         * Invoking a method on an object implies invoking code with access to the object.
+         */
         @Override
         protected void finalize() {
             System.out.println("See, I got hands on -> " + this);
+            /** This method is from the superclass */
             criticalAction();
         }
     }
     
     public static void main(String[] args) throws InterruptedException {
-
+        /** Making the subclass an inner class
+         * allows to store the reference by the still accessible outer class, FinalizerAttackExample.
+         */
         try {
             new MaliciousSubclass();
         } catch(SecurityException ex) {
             System.out.println("I wouldn't get hands on a ResourceClass instance.");
         }
         System.gc();
+        /** Sleeping periodically helps to demonstrate the non-deterministic behaviour */
         Thread.sleep(2000);
     }
 }
