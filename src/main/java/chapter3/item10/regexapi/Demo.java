6ec27ca0b221a11782da6b0aa47c9c4242a3e955
Rahul <rahul.samaranayake@zendesk.com>
2022-12-14 20:57:32 +1100

    regex with Pattern example added to item 10

--- src/main/java/chapter3/item10/regexapi/Demo.java
+++ src/main/java/chapter3/item10/regexapi/Demo.java
@@ -1,4 +1,20 @@
 package chapter3.item10.regexapi;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Demo {
+    public static void main(String[] args) {
+        System.out.println(runRegExAgainstStr("^(.+)@(.+)$", "user@domain.com"));
+        System.out.println(runRegExAgainstStr("^(.+)@(.+)$", "foo"));
+        System.out.println(runRegExAgainstStr("^(.+)@(.+)$", "123"));
+    }
+
+    public static boolean runRegExAgainstStr(String regex, String text) {
+        /** Pattern is a compiled regex that returns a Pattern object. */
+        Matcher matcher = Pattern.compile(regex).matcher(text);
+        /** Matcher interprets the pattern **/
+        return matcher.find();
+    }
 }
+
