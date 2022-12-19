02fd7593d550f6295e0957efb9856ad2713ad4c0
Rahul <rahul.samaranayake@zendesk.com>
2022-12-19 21:25:28 +1100

    enum logical equality demo

--- src/main/java/chapter3/item10/logicalequalityinenums/Demo.java
+++ src/main/java/chapter3/item10/logicalequalityinenums/Demo.java
@@ -1,5 +1,18 @@
 package chapter3.item10.logicalequalityinenums;
 
+enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+    THURSDAY, FRIDAY, SATURDAY
+}
+
+enum Day2 {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+    THURSDAY, FRIDAY, SATURDAY
+}
+
 public class Demo {
-    
+    public static void main(String[] args) {
+        /** There's no logical equality as there's no object identity. */
+        System.out.println(Day.SUNDAY.equals(Day2.SUNDAY));
+    }
 }
