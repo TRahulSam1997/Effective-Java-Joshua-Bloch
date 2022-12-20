5740b9fe94c1f64f7b77f250745a0a4a6988556e
Rahul <rahul.samaranayake@zendesk.com>
2022-12-20 18:29:21 +1100

    reflexvity demo added

--- src/main/java/chapter3/item10/equivalencerelation/reflexivity/Demo.java
+++ src/main/java/chapter3/item10/equivalencerelation/reflexivity/Demo.java
@@ -1,5 +1,15 @@
 package chapter3.item10.equivalencerelation.reflexivity;
 
-public class Demo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Demo {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("java");
+
+        /** The contains method checks if the element is contained in the list */
+        /** The returned "true" proves reflexivity as it's looking for the object (or element) in the collection. */
+        System.out.println(list.contains("java"));
+    }
 }
