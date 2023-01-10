51bbb935e3ea0baba54ea0c209034494d72ceb5b
Rahul <rahul.samaranayake@zendesk.com>
2023-01-10 22:44:47 +1100

    creating a high quality equals method with class field comparison updates

--- src/main/java/chapter3/item10/highqualityequalsmethod/objectsequals/Person.java
+++ src/main/java/chapter3/item10/highqualityequalsmethod/objectsequals/Person.java
@@ -22,7 +22,27 @@ class Person {
 
         Person p = (Person) o;
 
-        // Compare fields using the Objects.equals method
+        /** Compare fields using the Objects.equals() method. */
         return Objects.equals(name, p.name) && age == p.age;
+
+        /**
+         * "The Objects.equals() method is a utility method provided by the java.util.Objects class in Java.
+         * It can be used to compare the equality of two objects, taking into account that either or both objects can be null.
+         * The method will return true if both objects are null,
+         * or if the objects are non-null and the equals method of the first object returns true when called with the second object as the argument."
+         * - ChatGPT
+         */
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("John", 25);
+        Person p2 = new Person("John", 25);
+        Person p3 = new Person("Jane", 25);
+        Person p4 = new Person(null, 25);
+
+        System.out.println(p1.equals(p2)); // true
+        System.out.println(p1.equals(p3)); // false
+        System.out.println(p1.equals(p4)); // false
+
     }
 }
