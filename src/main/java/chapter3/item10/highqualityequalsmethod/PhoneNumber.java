32f3261df636f274b534158e1d65bb9b704cd9a3
Rahul <rahul.samaranayake@zendesk.com>
2023-01-11 18:41:36 +1100

    Final exmaple by Joshua Bloch for a perfect equals method

---
+++ src/main/java/chapter3/item10/highqualityequalsmethod/PhoneNumber.java
@@ -0,0 +1,33 @@
+package chapter3.item10.highqualityequalsmethod;
+
+/**
+ * Code taken from - https://github.com/jbloch/effective-java-3e-source-code/blob/master/src/effectivejava/chapter3/item10/PhoneNumber.java
+ * Class with a typical equals method (Page 48)
+ */
+public final class PhoneNumber {
+    private final short areaCode, prefix, lineNum;
+
+    public PhoneNumber(int areaCode, int prefix, int lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "area code");
+        this.prefix   = rangeCheck(prefix,   999, "prefix");
+        this.lineNum  = rangeCheck(lineNum, 9999, "line num");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max)
+            throw new IllegalArgumentException(arg + ": " + val);
+        return (short) val;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof PhoneNumber))
+            return false;
+        PhoneNumber pn = (PhoneNumber)o;
+        return pn.lineNum == lineNum && pn.prefix == prefix
+                && pn.areaCode == areaCode;
+    }
+
+    // Remainder omitted - note that hashCode is REQUIRED (Item 11)!
+}
