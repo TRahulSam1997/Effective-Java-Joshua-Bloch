7c0ce0269f651a42c791a88da2114b1116836945
Rahul <rahul.samaranayake@zendesk.com>
2023-01-29 21:48:25 +1300

    hashCode demo added for item 11

---
+++ src/main/java/chapter3/item11/hashcodedemo/Hash.java
@@ -0,0 +1,25 @@
+package chapter3.item11.hashcodedemo;
+
+/**
+ * Example courtesy of Educative.
+ * HashCode demo.
+ */
+class Hash {
+    public static void main(String[] args){
+        String a = "200";
+        String b = "200";
+
+        if(a.equals(b)){
+            System.out.println("Equal variables:");
+            System.out.println("a -> " + a.hashCode() + "\n" + "b -> " + b.hashCode());
+        }
+
+        String c = "10";
+        String d = "50";
+
+        if(!c.equals(d)){
+            System.out.println("\nUnequal variables:");
+            System.out.println("c -> " + c.hashCode() + "\n" + "d -> " + d.hashCode());
+        }
+    }
+}
