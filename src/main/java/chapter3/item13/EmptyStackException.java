b4a45625318b5f1b45e891bc5fca7641f4031b48
Rahul <rahul.samaranayake@zendesk.com>
2023-04-06 21:17:32 +1000

    Item 13 base clsses added

---
+++ src/main/java/chapter3/item13/EmptyStackException.java
@@ -0,0 +1,7 @@
+package chapter3.item13;
+
+/**
+ * Code taken from - https://github.com/jbloch/effective-java-3e-source-code/blob/master/src/effectivejava/chapter3/item13/EmptyStackException.java
+ */
+public class EmptyStackException extends IllegalStateException {
+}
