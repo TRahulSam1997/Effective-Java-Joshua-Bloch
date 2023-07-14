d7a955564f7766ddcbb61b898105103d918189cd
Rahul <rahul.samaranayake@zendesk.com>
2023-07-14 20:09:41 +1000

    thread safe clone method example for item 13

---
+++ src/main/java/chapter3/item13/threadsafe/SafeCounter.java
@@ -0,0 +1,34 @@
+package chapter3.item13.threadsafe;
+
+public class SafeCounter implements Cloneable {
+    private int count;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+
+    // This clone method is not thread-safe!
+    @Override
+    public SafeCounter clone() {
+        try {
+            return (SafeCounter) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(); // Can't happen
+        }
+    }
+
+//    // Thread safe
+//    @Override
+//    public synchronized SafeCounter clone() {
+//        try {
+//            return (SafeCounter) super.clone();
+//        } catch (CloneNotSupportedException e) {
+//            throw new AssertionError(); // Can't happen
+//        }
+//    }
+}
+
