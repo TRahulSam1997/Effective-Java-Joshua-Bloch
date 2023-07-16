c753a63d5391218309ea28eaead8e8a076826546
Rahul <rahul.samaranayake@zendesk.com>
2023-07-16 21:03:06 +1000

    Advantages of using copy constructor example

--- src/main/java/chapter3/item13/copyconstructor/Point.java
+++ src/main/java/chapter3/item13/copyconstructor/Point.java
@@ -12,10 +12,21 @@ public class Point {
         return y;
     }
 
-    // The clone method is extralinguistic, that is, it creates an object without using a constructor.
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    // The clone method is extralinguistic, that is, it creates an object without using a constructor
+    // Unenforceable adherence to thinly documented conventions.
+    @Override
+    public Point clone() {
+        try {
+            // No cast is needed because our clone method returns Point
+            return (Point) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(); // Can't happen
+        }
     }
 
     // This is a copy constructor. It creates a new Point that is a copy of an existing Point.
@@ -37,4 +48,63 @@ public class Point {
         }
     }
 
+}
+
+// Can create conflicts for clone() if final fields are in a class.
+final class ImmutablePoint {
+    private final int x;
+    private final int y;
+
+    public ImmutablePoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public ImmutablePoint clone() {
+        try {
+            // super.clone() won't work properly because of final fields.
+            return (ImmutablePoint) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(); // Can't happen
+        }
+    }
+
+    public static void main(String[] args) {
+        ImmutablePoint original = new ImmutablePoint(1, 2);
+        // This clone won't work.
+        ImmutablePoint cloned = original.clone();
+
+        System.out.println("Original X: " + original.getX() + ", Y: " + original.getY());
+        System.out.println("Cloned X: " + cloned.getX() + ", Y: " + cloned.getY());
+    }
+}
+
+// The clone() method in the Object class returns an Object, so we have to cast it to the appropriate type.
+class MyClass implements Cloneable {
+    int id;
+    String name;
+
+    public MyClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        MyClass obj1 = new MyClass(1, "Test");
+        MyClass obj2 = (MyClass) obj1.clone(); // casting
+    }
 }
